3ef9e442756d1dfd54d3ebce829048d98ee11b82
AnaisRevellat <anaisrevellat@gmail.com>
2022-09-03 13:37:05 +0200

    webpack config and installation done

---
+++ dist/main.bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+const test = '123';
+const alpha = 7;
+
+/******/ })()
+;
+//# sourceMappingURL=main.bundle.js.map
